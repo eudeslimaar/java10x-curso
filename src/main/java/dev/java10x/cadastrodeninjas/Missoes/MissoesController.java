8e8f1d65c28f04c1f9f3a5adcbe9577721a2e9c1
Eudes Lima <eudesfilho1994@outlook.com>
2025-12-18 17:59:25 -0300

    Refactor: Muda nome do controller que para MissoesController

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/missoes")
-public class MissaoController{
+public class MissoesController {
 
     @GetMapping
     public String missoes() {
