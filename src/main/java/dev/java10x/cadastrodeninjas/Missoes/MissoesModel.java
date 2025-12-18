11693da22b9810dac5e9dbcc92a47d964fe31b04
Eudes Lima <eudesfilho1994@outlook.com>
2025-12-18 17:21:25 -0300

    feat: Coloca Lombok Na class NinjaModel e MissoesModel para abstração

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesModel.java
@@ -1,13 +1,20 @@
 package dev.java10x.cadastrodeninjas.Missoes;
 
-import dev.java10x.cadastrodeninjas.Ninjas.Ninja;
+import dev.java10x.cadastrodeninjas.Ninjas.NinjaModel;
 import dev.java10x.cadastrodeninjas.Niveis.Difficult;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missao")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,30 +22,5 @@ public class MissoesModel {
     private String name;
     private Difficult difficult;
     @OneToMany(mappedBy = "missoes" )
-    private List<Ninja> ninjas;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String name, Difficult difficult) {
-        this.id = id;
-        this.name = name;
-        this.difficult = difficult;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Difficult getDifficult() {
-        return difficult;
-    }
-
-    public void setDifficult(Difficult difficult) {
-        this.difficult = difficult;
-    }
+    private List<NinjaModel> ninjas;
 }
---
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -0,0 +1,29 @@
+package dev.java10x.cadastrodeninjas.Ninjas;
+
+import dev.java10x.cadastrodeninjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private int idade;
+
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")
+    private MissoesModel missoes;
+}
